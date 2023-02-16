2d3c8e8ecad2123584e89f86d83458df36c0a0b4
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-02-16 23:59:11 +0100

    prog C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+	return (0);
+}
