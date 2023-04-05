39e089f809b2697ace4ff1d4876c45eb88305bd3
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-04-05 20:53:24 +0000

    C

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t res = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num++;
+		res++;
 		h = h->next;
 	}
 
-	return (num);
+	return (res);
 }
