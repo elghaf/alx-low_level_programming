d61d231e55c941ba0a331c4e4eb95bd981ddc641
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-04-05 21:19:40 +0000

    'C'

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,17 +9,17 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *node_new;
+	listint_t *new;
 
-	node_new = malloc(sizeof(listint_t));
-	if (!node_new){
+	new = malloc(sizeof(listint_t));
+	if (!new){
 		return (NULL);
 	}
 		
 
-	node_new->n = n;
-	node_new->next = *head;
-	*head = node_new;
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
-	return (node_new);
+	return (new);
 }
