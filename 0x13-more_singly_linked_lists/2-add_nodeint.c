cfa12a6a14b6960d19ed90ac4e3e1465d4b819fa
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-04-05 23:58:57 +0000

    'C'

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,27 +1,27 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list
- * @head: pointer to the first node in the list
- * @n: data to insert in that new node
- *
- * Return: pointer to the new node, or NULL if it fails
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: pointer to the first element of the list.
+ * @n: int to set in the new node.
+ * Return: NULL if it fails
+ * or address of element
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
-// to allocate memory for the new node created
+listint_t *new; /* declare a new pointer to a listint_t struct */
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
+/* allocate memory for the new node */
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL); /* return NULL if memory allocation fails */
 
-// set the value of n in the new node created
-// to understand the concepte read about array links
-	new->n = n;
-	new->next = *head;
-// update head pointer to the point of node created
-	*head = new;
-// finaly return the value pointer in the new nodes
-	return (new);
+/* set the value of n in the new node */
+new->n = n;
+new->next = *head;
+
+/* update head pointer to point to the new node */
+*head = new;
+/* return a pointer to the new node */
+return (new);
 }
