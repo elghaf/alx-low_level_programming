3f49b8d34095bbcc53c4ffd639113149cc83e7d5
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-04-05 21:02:01 +0000

    'C'

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,15 +1,21 @@
 #include "lists.h"
 
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
 
-size_t listint_len(const listint_t *h){
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
 
-    size_t res = 0;
-
-    while (h != NULL){
-        printf("%d\n", h->n);
-
-        res++;
-        h = h->next;
-    }
-    return (res);
+	return (num);
 }
+
