d2886fd7445332c4208d17937fe3c6c2cfe002a9
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-04-06 00:01:24 +0000

    C

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * add_nodeint - create node at the beginning of a linked lists.
- * @head: element pointer node in the lists
- * @n: input to insert in that new nodes.
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in that new node
  *
  * Return: pointer to the new node, or NULL if it fails
  */
