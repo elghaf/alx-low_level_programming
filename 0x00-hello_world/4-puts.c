46629799e042d84c41e4ef75eb323e8efaacf89a
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-17 18:40:38 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * Files: 4-puts.c
+ * Authour: El ghafraoui
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Program starts here
+ *
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
