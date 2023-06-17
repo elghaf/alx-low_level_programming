db099a9142ea1866c0550538474b61f09dce3eb5
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-17 18:42:46 +0100

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * File:5-printf.c
+ * Authour: El ghafraoui
+ */
+
+#include <stdio.h>
+
+/*
+ * main - prints ("with proper grammar, but the outcome is a piece of art,").
+ *
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
