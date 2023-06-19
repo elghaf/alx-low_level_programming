1e6da55380b23aacb90f7ca7822df99b0705702e
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-19 19:56:41 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints the alphabet in lowercase, followed by a new line
+ * Description: alphabet in lowercase, followed by a new line
  *
  * Return: Always 0 (Success)
  */
