8a64036abe3f9ccf9645a7b51c02e8596f621e5f
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-19 19:55:19 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,17 +3,19 @@
 /**
  * main - Entry point
  *
+ * Description: Prints the alphabet in lowercase, followed by a new line
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char lowercase_letter;
+	char lowercase_letter;
 
-    for (lowercase_letter = 'a'; lowercase_letter <= 'z'; lowercase_letter++)
-    {
-        putchar(lowercase_letter);
-    }
+	for (lowercase_letter = 'a'; lowercase_letter <= 'z'; lowercase_letter++)
+	{
+		putchar(lowercase_letter);
+	}
 
-    putchar('\n');
-    return 0;
+	putchar('\n');
+	return 0;
 }
