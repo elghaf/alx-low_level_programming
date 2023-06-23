39a435607c585a58754a03bce7f0a040105b0d56
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-23 14:51:06 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,9 +6,9 @@
  *
  * Return: 1 if @character is a letter, 0 otherwise.
  */
-int _isalpha(int character)
+int _isalpha(int charact)
 {
-	if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z'))
+	if ((charact >= 'a' && charact <= 'z') || (charact >= 'A' && charact <= 'Z'))
 		return (1);
 	else
 		return (0);
