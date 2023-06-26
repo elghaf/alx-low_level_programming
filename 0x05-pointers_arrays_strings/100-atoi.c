173bee5579bcc1c2fe2a01fffc18a0428a2b244f
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-26 17:05:09 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,37 +8,37 @@
  */
 int _atoi(char *str)
 {
-    int index = 0;
-    int minusCount = 0;
-    int number = 0;
-    int length = 0;
-    int foundDigit = 0;
-    int digitValue = 0;
+int index = 0;
+int minusCount = 0;
+int number = 0;
+int length = 0;
+int foundDigit = 0;
+int digitValue = 0;
 
-    while (str[length] != '\0')
-        length++;
+while (str[length] != '\0')
+length++;
 
-    while (index < length && foundDigit == 0)
-    {
-        if (str[index] == '-')
-            ++minusCount;
+while (index < length && foundDigit == 0)
+{
+if (str[index] == '-')
+++minusCount;
 
-        if (str[index] >= '0' && str[index] <= '9')
-        {
-            digitValue = str[index] - '0';
-            if (minusCount % 2)
-                digitValue = -digitValue;
-            number = number * 10 + digitValue;
-            foundDigit = 1;
-            if (str[index + 1] < '0' || str[index + 1] > '9')
-                break;
-            foundDigit = 0;
-        }
-        index++;
-    }
+if (str[index] >= '0' && str[index] <= '9')
+{
+digitValue = str[index] - '0';
+if (minusCount % 2)
+digitValue = -digitValue;
 
-    if (foundDigit == 0)
-        return 0;
+number = number * 10 + digitValue;
+foundDigit = 1;
+if (str[index + 1] < '0' || str[index + 1] > '9')
+break;
 
-    return number;
+foundDigit = 0;
+}
+index++;
+}
+if (foundDigit == 0)
+return 0;
+return number;
 }
