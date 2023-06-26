47513fe90b9f5423bb343b9332d05d2159a8fc05
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-26 16:54:49 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,17 +8,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int length = 0;
-    int x = 0;
-
-    while (*(src + length) != '\0')
-    {
-        length++;
-    }
-    for (; x < length; x++)
-    {
-        dest[x] = src[x];
-    }
-    dest[length] = '\0';
-    return dest;
+int length = 0;
+int x = 0;
+while (*(src + length) != '\0')
+{
+length++;
+}
+for (; x < length; x++)
+{
+dest[x] = src[x];
+}
+dest[length] = '\0';
+return dest;
 }
