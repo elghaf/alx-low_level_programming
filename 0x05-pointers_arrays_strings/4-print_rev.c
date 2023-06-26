3b26f680f94510e3c47bbe9c29b9c9f381030915
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-26 16:01:30 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,18 +6,18 @@
  */
 void print_rev(char *str)
 {
-    int length = 0;
-    int index;
-    int j;
+	int length = 0;
+	int index;
+	int j;
 
-    while (str[length] != '\0')
-    {
-        length++;
-    }
-    index = length - 1;
-    for (j = length; j > 0; j--)
-    {
-        _putchar(str[index--]);
-    }
-    _putchar('\n');
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	index = length - 1;
+	for (j = length; j > 0; j--)
+	{
+		_putchar(str[index--]);
+	}
+	_putchar('\n');
 }
