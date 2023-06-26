d248ce0b097c8015bd9b3ba45db9ea2295dc46d1
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-26 16:12:12 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,16 +6,14 @@
  */
 void rev_string(char *str)
 {
-    int len = 0;
-    int i;
-
-    while (str[len] != '\0')
-        len++;
-
-    for (i = 0; i < len / 2; i++)
-    {
-        char temp = str[i];
-        str[i] = str[len - i - 1];
-        str[len - i - 1] = temp;
-    }
+int len = 0;
+int i;
+while (str[len] != '\0')
+    len++;
+for (i = 0; i < len / 2; i++)
+{
+    char temp = str[i];
+    str[i] = str[len - i - 1];
+    str[len - i - 1] = temp;
+}
 }
