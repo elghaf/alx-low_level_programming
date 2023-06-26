f22bff121e17da745468e1a8e3a95b6a76655e5d
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-26 16:46:29 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,13 +8,14 @@
  */
 void print_array(int *arr, int length)
 {
-int i;
-for (i = 0; i < length ; i++)
-{
-if (i != length - 1)
-    printf("%d, ", arr[i]);
-else
-    printf("%d", arr[i]);
-}
-printf("\n");
+    int i;
+
+    for (i = 0; i < length; i++)
+    {
+        if (i != length - 1)
+            printf("%d, ", arr[i]);
+        else
+            printf("%d", arr[i]);
+    }
+    printf("\n");
 }
