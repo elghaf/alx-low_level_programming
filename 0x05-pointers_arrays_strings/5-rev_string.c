4971cc4072aa51fc8a87ead026dcb2e60eeb4634
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-26 16:10:32 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,19 +4,18 @@
  * rev_string - reverses a string
  * @str: the string to reverse
  */
-
 void rev_string(char *str)
 {
-	int len = 0;
-	int i;
+    int len = 0;
+    int i;
 
-	while (str[len] != '\0')
-		len++;
+    while (str[len] != '\0')
+        len++;
 
-	for (i = 0; i < len / 2; i++)
-	{
-		char temp = str[i];
-		str[i] = str[len - i - 1];
-		str[len - i - 1] = temp;
-	}
+    for (i = 0; i < len / 2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
 }
