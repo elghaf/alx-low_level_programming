88d53cbb356d2ea94b8f70649b0a89051bf1a031
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-30 20:15:07 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,10 +2,18 @@
 
 /**
  * _strcmp - Compares two strings.
- * @first_string: First string to compare.
- * @second_string: Second string to compare.
+ * @first_string: Pointer to the first string.
+ * @second_string: Pointer to the second string.
  *
- * Return: 0 if the strings are equal, non-zero otherwise.
+ * Return: 0 if the strings are equal, a non-zero value otherwise.
+ *
+ * Description: This function compares the characters of two strings, pointed
+ *              to by first_string and second_string. It starts comparing the
+ *              characters until a null byte ('\0') is encountered in either
+ *              string or if the characters being compared are different.
+ *              If the strings are equal, it returns 0. Otherwise, it returns
+ *              a non-zero value representing the difference between the ASCII
+ *              values of the differing characters.
  */
 int _strcmp(char *first_string, char *second_string)
 {
