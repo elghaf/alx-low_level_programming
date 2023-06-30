8ded9a6b73fb97d79f865b35f1980432063a3c10
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-30 18:56:06 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenate also two strings.
+ * Description: concatenate strings.
+ * @first_string : destination string.
+ * @second_string : source string.
+ * @n : number of bytes from the second_string.
+ * 
+ **/
+
+char *_strncat(char *first_string, char *second_string, int n)
+{
+    int lenght_string = 0, i =0;
+    while (first_string[i++])
+    {
+        lenght_string++;
+    }
+    for ( i = 0; second_string[i] && i < n ; i++)
+    {
+        /* code */
+        first_string[lenght_string++] = second_string[i];
+    }
+    return (first_string);
+    
+    
+}
