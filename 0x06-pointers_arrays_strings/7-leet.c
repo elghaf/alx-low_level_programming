64d21b2e24af21e53da247bf4ed4d7d930ec0061
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-30 23:35:35 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,22 +10,23 @@
  *
  * Return: Pointer to the encoded string.
  */
-char *leet(char *str)
+
+char *leet(char *s)
 {
-char letters[] = "aAeEoOtTlL";
-char numbers[] = "43071";
+char *letters = "AEOTL";
+char *numbers = "43071";
 int i, j;
 
-for (i = 0; str[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 	for (j = 0; letters[j] != '\0'; j++)
 	{
-		if (str[i] == letters[j])
+		if (s[i] == letters[j] || s[i] == letters[j] + 32)
 		{
-			str[i] = numbers[j];
+			s[i] = numbers[j];
 			break;
 		}
 	}
 }
-return str;
+return s;
 }
