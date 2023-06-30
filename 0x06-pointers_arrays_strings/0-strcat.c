309c66c3ee002c69f8ca1a4d199fcdc3f5b33799
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-30 18:44:28 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,26 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two var.
- * @first_string: contain string to concatenat with 
- * @second_string: second string
- * Return: pointer to resulting string.
+ * _strcat - Concatenates two strings.
+ * @first_string: Pointer to the first string.
+ * @second_string: Pointer to the second string.
+ *
+ * Return: Pointer to the resulting string.
  */
-char *_strcat(char *first_string,char *second_string)
+char *_strcat(char *first_string, char *second_string)
 {
-int first = 0,second = 0;
-while(first_string[first])
-{
-first++;
-}
-for (second=0; second_string[second] != 0;second++)
-{
-first_string[first] = second_string[second];
-first++;
-}
-first_string[first] = '\0';
-return(first_string);
+	int first = 0, second = 0;
+
+	while (first_string[first])
+	{
+		first++;
+	}
+
+	for (second = 0; second_string[second] != 0; second++)
+	{
+		first_string[first] = second_string[second];
+		first++;
+	}
+	first_string[first] = '\0';
+	return (first_string);
 }
