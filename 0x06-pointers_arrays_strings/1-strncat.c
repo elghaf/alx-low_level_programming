e46c4967f5fc6dc6795cb89d50fe7a19b1d10dca
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-30 19:00:07 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
- * *_strncat - concatenate also two strings.
- * Description: concatenate strings.
- * @first_string : destination string.
- * @second_string : source string.
- * @n : number of bytes from the second_string.
- * 
- **/
+ * _strncat - Concatenates two strings with a specified length.
+ * @first_string: Pointer to the destination string.
+ * @second_string: Pointer to the source string.
+ * @n: Number of bytes to concatenate from the second string.
+ *
+ * Return: Pointer to the resulting string.
+ */
 char *_strncat(char *first_string, char *second_string, int n)
 {
-int lenght_string = 0, i =0;
+	int length_string = 0, i = 0;
 
-while (first_string[i++])
-{
-lenght_string++;
-}
-for ( i = 0; second_string[i] && i < n ; i++)
-{
-first_string[lenght_string++] = second_string[i];
-}
-return (first_string); 
+	while (first_string[i++])
+	{
+		length_string++;
+	}
+
+	for (i = 0; second_string[i] && i < n; i++)
+	{
+		first_string[length_string++] = second_string[i];
+	}
+
+	return (first_string);
 }
