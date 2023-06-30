bbe03d3d0a0d2881ed486eba868ab3db12a7021d
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-06-30 23:34:00 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,31 @@
 #include "main.h"
 
 /**
- * *leet - encodes a string to 1337
- * Description: changes some letters to numbers
- * Return: edited string
- * @s: input string
- **/
-
-char *leet(char *s)
+ * leet - encodes a string to 1337
+ * @str: input string to encode
+ *
+ * Description: Encodes a string by replacing certain letters with numbers.
+ * Only the characters 'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', and 'L'
+ * are replaced with '4', '3', '0', '7', '1' respectively.
+ *
+ * Return: Pointer to the encoded string.
+ */
+char *leet(char *str)
 {
-char *letters = "AEOTL";
-char *numbers = "43071";
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "43071";
 int i, j;
 
-for (i = 0; s[i] != '\0'; i++)
+for (i = 0; str[i] != '\0'; i++)
 {
 	for (j = 0; letters[j] != '\0'; j++)
 	{
-		if (s[i] == letters[j] || s[i] == letters[j] + 32)
+		if (str[i] == letters[j])
 		{
-			s[i] = numbers[j];
+			str[i] = numbers[j];
 			break;
 		}
 	}
 }
-return s;
+return str;
 }
