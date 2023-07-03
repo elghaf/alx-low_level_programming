fcab039f5c4b721df1cf1737062b084015fc1743
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-03 20:33:21 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,26 @@
 #include "main.h"
 /**
  * _strspn - Entry point
- * @s: input
- * @accept: input
+ * @s: input our character.
+ * @accept: innput to accepts.
  * Return: Always 0 (Success)
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n = 0;
-    int found;
+    unsigned int n = 0;
+    int lookup[256] = {0};
 
-    while (*s)
+    while (*accept)
     {
-        found = 0;
-        for (char *a = accept; *a; a++)
-        {
-            if (*s == *a)
-            {
-                found = 1;
-                break;
-            }
-        }
-        if (!found)
-            return n;
+        lookup[(unsigned char)*accept] = 1;
+        accept++;
+    }
+    while (*s && lookup[(unsigned char)*s])
+    {
         n++;
         s++;
     }
+    
     return n;
 }
