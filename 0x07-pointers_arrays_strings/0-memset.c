3f057e4a51b21b7bf5ca5110a12fd21504ea7fd0
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-03 19:50:05 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,8 @@
 /**
  * _memset - Fills a block of memory with a specified value.
- *
- * @destination: Pointer to the memory block to be filled.
  * @value: The value to be set.
  * @num_bytes: Number of bytes to be filled.
- *
+ * @destination: Pointer to the memory block to be filled.
  * Return: Pointer to the filled memory block (@destination).
  */
 char *_memset(char *destination, char value, unsigned int num_bytes)
