7fc7a5f918696651db73572fbfff2e8133bdbd09
elghaf <ay.elghafraoui@gmail.com>
2023-07-08 09:53:12 -0700

    C

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main - Prints the multiplication of two argument numbers
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0
+ * Description: This program takes two
+ *  numbers as command-line arguments and performs
+ **/
+int main(int c_argc, char *v_argv[])
+{
+int multi;
+
+if(c_argc == 3)
+{
+multi = atoi(v_argv[c_argc -1]) * atoi(v_argv[c_argc-2]);
+printf("%d", multi);
+printf("\n");
+return(0);
+}
+else
+{
+printf("error\n");
+return (1);
+}
+}
