8fbdce82188cf1b60c511676081a66d811f16aad
elghaf <ay.elghafraoui@gmail.com>
2023-07-08 07:19:12 -0700

    C

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * main - a program that print the number of input arguments
  * @c_argc : count Number of argument input
  * @v_argv : string of number argument input
- * Return : Succes
+ * Return : Success
  * Description : program that print the number of arg string
  **/
 int main(int c_argc, char *v_argv[]__attribute__((unused)))
