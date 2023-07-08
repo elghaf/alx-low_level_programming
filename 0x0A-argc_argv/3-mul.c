5efbe03bb2e2b45cd919a1f7ac9326296600a540
elghaf <ay.elghafraoui@gmail.com>
2023-07-08 07:45:53 -0700

    C

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * main - the program that mutiplies two numbers
+ * @c_argc : the count of input number
+ * @v_argv : the array of the input number
+ * Return : Success
+ * Description : program that take the arg and do 
+ * multulication
+ **/
+int main (int c_argc, char *v_argv[])
+{
+	int i=0;
+
+	for(i =0;i<c_argc; c++)
+	{
+		printf("%c", v_argv[i]);
+	}
+	return (0);
+}
