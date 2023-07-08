fd9f2349bfea1c608c67320d1ed256b92b106a3b
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-08 18:14:43 +0100

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - program that adds numbers positives
+ * @c_argc : Number of arg
+ * @v_argv : string of arg
+ * Return : Success
+ */
+int main(int argc, char *argv[])
+{
+    int i;
+    unsigned int j, sum = 0;
+    char *arg;
+
+    if (argc > 1)
+    {
+        for (i = 1; i < argc; i++)
+        {
+            arg = argv[i];
+            for (j = 0; j < strlen(arg); j++)
+            {
+                if (!isdigit(arg[j]))
+                {
+                    printf("Error: Invalid argument\n");
+                    return 1;
+                }
+            }
+            sum += atoi(arg);
+        }
+        printf("Sum: %d\n", sum);
+    }
+    else
+    {
+        printf("Sum: 0\n");
+    }
+    return 0;
+}
