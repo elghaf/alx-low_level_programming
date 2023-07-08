f65f4957590efcb1ef7c7663598fd80dd808988a
elghaf <ay.elghafraoui@gmail.com>
2023-07-08 07:02:20 -0700

    C

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include "main.h"
+#include "stdio.h"
 
-/*
- * main - print the name argv \n
- * @c_argc : the argument of len of the array
- * @v_argv : the argument of array 
- * Return : succ
- * */
+/**
+ * main - A program that prints the name that been added.
+ * @c_argc: Argument of the len of the array 
+ * @v_argv: array value
+ * Return: Success
+ */
+
 int main(int c_argc __attribute__((unused)), char *v_argv[])
 {
 printf("%s\n", v_argv[0]);
