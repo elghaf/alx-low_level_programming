de01ef02afdf135a152196e461150cbb51b394cb
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-17 22:35:36 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,12 @@
  */
 void print_dog(struct dog *d)
 {
-char name, owner;
-int age;
+	if (d == NULL)
+		return;
 
-name = d->name;
-owner = d->owner;
-age = d->age;
-printf("Name: %s", name);
-printf("Age: %d", age);
-printf("Owner: %s", owner);
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
