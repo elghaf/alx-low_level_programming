f6eedaaa63b183ad7011e2f5fb5a17eb710d3057
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-17 21:28:21 +0100

    add

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dogs information
+ * @name: name dog
+ * @age: Age of the dog
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+    /* data */
+    char *name;
+    float age;
+    char *owner;
+};
+
+
