1c31a619b5df0fe5804ee622acb00476174f499a
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-17 21:52:15 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,17 @@ struct dog
     char *owner;
 };
 
+/**
+ * dog_t - dog struct.
+ */
+typedef struct  dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
