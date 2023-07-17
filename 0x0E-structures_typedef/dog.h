bad694d8390ec6c08013a7028539049111a73b36
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-17 21:59:18 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,11 +14,6 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-#endif
-#ifndef _FUNCTIONS_H
-#define _FUNCTIONS_H
-int _putchar(char c);
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
