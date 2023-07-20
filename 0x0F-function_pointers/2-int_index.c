4a2e0ecf1b40174c50237d72e566a40d2f23e51f
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-21 00:35:34 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,22 +1,23 @@
 #include "function_pointers.h"
 /**
- * int_index - return index place if comparison = true, else -1
- * @array: array
- * @size: size of elements in array
- * @cmp: pointer to func of one of the 3 in main
+ * int_index - returning index place
+ * @array: array of elem
+ * @size: size of elem in arr
+ * @cmp: pointer to function
  * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+unsigned int i;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]))
-			return (i);
-	}
+if (array == NULL || size <= 0 || cmp == NULL)
 	return (-1);
+
+while(i < size)
+{
+	if (cmp(array[i]))
+	return (i);
+    i++;
+}
+return (-1);
 }
