f312f8c2e64deda9dc5b69c053bf36e6b1e24470
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-21 00:06:35 +0100

    Update 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio>
+/**
+ * array_iterator - print array elements
+ * @array: array of elem
+ * @size: the size of arr
+ * @action: function pointer
+ * Return: void none
+ * 
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+unsigned int i;
+if(action == NULL || array == NULL)
+return ;
+while (i < size)
+{
+/* code */
+action(array[i]);
+i++;
+}
+}
