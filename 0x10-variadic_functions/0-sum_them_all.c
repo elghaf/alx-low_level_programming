3fa8056b7976f9bf6ebf6a457ff66244c05865df
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-22 00:07:51 +0100

    add

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of param.
+ * @n: The n of param in the func.
+ * @...: var number of param.
+ * Return: if n Null.
+ *      the sum of all params.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list ap;
+    unsigned int i = 0, som = 0;
+
+    var_start(ap, n);
+    while (i < n)
+    {
+        /* code */
+        som = som + va_arg(ap, int);
+    }
+    va_end(ap);
+    return (som):    
+}
