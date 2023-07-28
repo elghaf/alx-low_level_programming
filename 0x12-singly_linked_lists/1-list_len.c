da8cc622189a897e408b32fd3852122497a67ccb
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-28 17:43:24 +0100

    add

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - size_t the number of elements in a linked list
+ * @h: pointer to the list_t.
+ *
+ * Return: number of elements in h
+ */
+
+size_t list_len(const list_t *h)
+{
+    const list_t *result = h;
+    size_t index = 0;
+
+    while (result != NULL)
+    {
+        /* code */
+        index++;
+        resul = result->next;
+    }
+    return (index);
+    
+}
