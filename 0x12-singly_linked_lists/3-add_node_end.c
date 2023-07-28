6e19f00c9ab8c01c6be04479fd0380ece72e09fe
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-28 20:24:52 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,8 +4,11 @@
 #include "lists.h"
 
 /**
- * add_node_end - function that adds a news node
- * 
+ * add_node_end - adds a node at end of (list_t)SLL
+ * @head: double pointer head.
+ * @str: input str to add.
+ *
+ * Return: new node address.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
@@ -25,6 +28,21 @@ list_t *add_node_end(list_t **head, const char *str)
     /*add node to the last not beginin*/
     node->len = index;
     node->str = strdup(str);
-    if(*head == )
-    
+    if(*head == NULL)
+    {
+        node->next = *head;
+        *head = node;
+    }
+    else
+    {
+        node->next = NULL;
+        last_node = *head;
+        while (last_node->next)
+        {
+            /* code */
+            last_node = last_node->next;
+        }
+        last_node->next = node;
+    }
+    return (node);    
 }
