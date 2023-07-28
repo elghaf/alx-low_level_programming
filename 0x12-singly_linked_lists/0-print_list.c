ed8714704821b97f780fe1b03b2b8663c188adbd
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-28 17:05:28 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,8 +20,8 @@ size_t print_list(const list_t *h)
         else
         {
             printf("[0] (nil)");
+            printf("\n");
         }
-        printf("\n");
         index++;
         result = result->next;
     }
