99191a0e0ef9617641754a1bf10d4c0daf706a31
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-31 18:34:17 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,3 +7,18 @@
  * Return: The number of terms linked list.
  * 
  */
+size_t print_listint(const listint_t *h)
+{
+    int i = 0;
+    listint_t const *chois;
+
+    chois = h;
+    while (chois != NULL)
+    {
+        /* code */
+        printf("%d\n", chois->n);
+        i++;
+        chois = chois->next;
+    }
+    return (i);
+}
