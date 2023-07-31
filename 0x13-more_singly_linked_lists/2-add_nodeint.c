f6e20cf91d21ac2a62a2c13fbbda8552d499ea35
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-07-31 23:13:47 +0100

    add

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
-  * add_nodeint - Adding the node at the beginning
-  * @head: The pointer to the starting node of a linked list
-  * @n: The number or data in a linked list
+  * add_nodeint - add nodeint at the beg
+  * @head: pointer start node of a linked list
+  * @n: data in a linked list
   * Return: The pointer to a newly created node
   */
 listint_t *add_nodeint(listint_t **head, const int n)
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,8 +3,8 @@
 /**
   * add_nodeint_end - Adding the node at the end
   * @head: The pointer to the starting node of a linked list
-  * @n: The number or data in a linked list
-  * Return: The pointer to a newly created node
+  * @n: n number in a linked list
+  * Return: the address pointer to a newly created node
   */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
@@ -22,17 +22,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	add_new_node_end->next = NULL;
 
 	if (*head == NULL)
-	{
 		*head = add_new_node_end;
-	}
 	else
 	{
 		chois = *head;
 
 		while (chois->next != NULL)
-		{
 			chois = chois->next;
-		}
 		chois->next = add_new_node_end;
 	}
 	return (add_new_node_end);
