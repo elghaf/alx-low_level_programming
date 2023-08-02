0d3b861e5f2e09796a7532e5b8535969c382a24a
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-02 02:02:46 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -16,9 +16,9 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
-		temp = *head;
-		free(chois);
+		chois = *head;
 		*head = (*head)->next;
+		free(chois);
 	}
 	*head = NULL;
 }
