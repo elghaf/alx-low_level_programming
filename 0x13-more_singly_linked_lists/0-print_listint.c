7675ea18de7e6c65a45b8bab1fea5179d6848e4b
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-02 01:49:07 +0100

    add

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,9 +10,10 @@
 size_t print_listint(const listint_t *h)
 {
     int i = 0;
-    listint_t const *chois = h;
+    listint_t const *chois;
 
-    while (h != NULL)
+    chois = h;
+    while (chois != NULL)
     {
         /* code */
         printf("%d\n", chois->n);
