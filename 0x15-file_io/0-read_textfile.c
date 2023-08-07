0fbbbab2f04003a729a14b366f24cdc9902f455e
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-07 22:47:38 +0100

    add

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read string text and prints it to te POSIX
+ * 
+ * 
+ * 
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
