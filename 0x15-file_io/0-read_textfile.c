dea08bfb5454d235c9301bffb9e2b86afa25ed41
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-07 22:55:27 +0100

    add

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,12 +3,27 @@
 
 /**
  * read_textfile - Read string text and prints it to te POSIX
- * 
- * 
- * 
+ * @filename: point of the file name
+ * @letters: letter to be read
+ * Return:  the actual number of letters,
+ *          it could read and print.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    
+    char *bufers;
+    ssize_t files, writ, re;
+
+    files = open(filename, O_RDONLY)
+    if (files == -1)
+    {
+        /* code */
+        return (0);
+    }
+    bufers = malloc(sizeof(char) * letters);
+    re = read(files, bufers, letters);
+    writ = write(STDOUT_FILENO, bufers, re);
+    free(bufers);
+    close(files);
+    return (writ);    
 }
