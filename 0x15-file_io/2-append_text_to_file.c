d74ff020f8ccf4f6fa6bb239ee7e1b2e731d9514
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-07 23:13:54 +0100

    Create 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - add text end of the file.
+ * @filename: A pointer to the name of the fle.
+ * @text_content: The string to add to the e of the file.
+ *
+ * Return: If the function fails -1.
+ *         Otherwise - 1.
+ */
+
+int append_text_to_file(const char *filename, char *text_content) 
+{
+    int file_descriptor, write_result, content_length = 0;
+
+    if (filename == NULL)
+        return (-1);
+
+    if (text_content != NULL) {
+        for (content_length = 0; text_content[content_length];)
+            content_length++;
+    }
+
+    file_descriptor = open(filename, O_WRONLY | O_APPEND);
+    write_result = write(file_descriptor, text_content, content_length);
+
+    if (file_descriptor == -1 || write_result == -1)
+        return (-1);
+
+    close(file_descriptor);
+    return (1);
+}
