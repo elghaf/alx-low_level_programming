95cad7fc9d4ce6436f6520cf95c8946dcad3557c
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-07 23:14:46 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,12 +12,13 @@
 int append_text_to_file(const char *filename, char *text_content) 
 {
     int file_descriptor, write_result, content_length = 0;
+    int i = 0;
 
     if (filename == NULL)
         return (-1);
 
     if (text_content != NULL) {
-        for (content_length = 0; text_content[content_length];)
+        for (i = 0; text_content[i];i++)
             content_length++;
     }
 
